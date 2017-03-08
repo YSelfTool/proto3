72729883c6aad2a6a091eabe066fa3ff8ff5eb8c
Robin Sonnabend <robin@fsmpi.rwth-aachen.de>
2017-03-08 02:13:43 +0100

    Show TOP-description
    On hovering and (with JS) in an element that opens on click.
    /close #43

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+
+function toggle_expansion() {
+    var description_element = document.getElementById(this.id + "-description");
+    var current_state = document.defaultView.getComputedStyle(description_element, "").getPropertyValue("display");
+    var new_state = current_state == "none" ? "block" : "none";
+    description_element.style.display = new_state;
+};
+
+window.onload=function() {
+    var buttons = document.getElementsByClassName("expansion-button");
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        console.log(button);
+        button.onclick = toggle_expansion;
+    }
+};
